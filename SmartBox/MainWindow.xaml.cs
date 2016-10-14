9296bb156bef4d83dbc6a4da00ffad354d00ad3f
Danielle Suurlant <danielle.suurlant@gmail.com>
2016-10-14 15:42:34 +0200

    working on #8

--- SmartBox/MainWindow.xaml.cs
+++ SmartBox/MainWindow.xaml.cs
@@ -14,6 +14,7 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 using Ookii.Dialogs.Wpf;
 using System.IO;
+using SQLite;
 
 namespace SmartBox
 {
@@ -22,9 +23,24 @@ namespace SmartBox
     /// </summary>
     public partial class MainWindow : Window
     {
+        private SQLiteConnection db;
+        private static string dbName = "gamecollection.db";
+
         public MainWindow()
         {
             InitializeComponent();
+            if (File.Exists(dbName) == false)
+            {                
+                // create db, init tables
+                System.Data.SQLite.SQLiteConnection.CreateFile(dbName);
+                db = new SQLiteConnection(dbName);
+                db.CreateTable<Entity.Game>();
+                db.CreateTable<Entity.Template>();
+                // @TODO issue #3 auto add DOSBox config templates.
+            } else
+            {
+                db = new SQLiteConnection(dbName);
+            }
             
         }
 
@@ -65,6 +81,12 @@ namespace SmartBox
         {
             var loadPath = SmartBox.Properties.Settings.Default.GameCollectionPath;
             var files = Directory.GetFiles(loadPath);
+
+            foreach (string filename in files)
+            {
+                Entity.Game game = new Entity.Game();
+
+            }
         }
         
     }
